e1f33c10a5394c7e8f7aedb19dd0223a4b0980c5
oriel <orielsilvajr@gmail.com>
2023-12-16 15:44:19 -0500

    fun stuff

--- code.js
+++ code.js
@@ -21,11 +21,39 @@ For example, if n is 5, the function should return the following array:
 
 hint: How many digits are in row n?
 
+Each array on index [0] = 1
+Each array with more than 1 number will have its last index = 1
+
+[First index = 1]
+[Last index = 1]
+
+
 */
 
 
 function generatePascalsTriangle(n) {
     //Your code here
+    //Edge case 1 & 2
+    if (n === 1) return [[1]]
+    if (n < 1) return [[]]
+
+    const main = [[1]];
+
+        for (let i = 1; i < n; i++) {
+
+            let pre = main[i - 1];
+            const curr = [];
+
+            curr.push(1)
+
+            for (let j = 1; j < pre.length; j++) {
+                curr[j] = pre[j] + pre[j - 1];
+            }
+
+            curr.push(1)
+            main.push(curr)
+        }
+    return main
 }
 
 
